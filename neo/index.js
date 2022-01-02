3146fb4533ff2e890e3bb22c74ef163147f29fbc
Pedejeca135 <pjco90@hotmail.com>
2022-01-02 10:13:35 -0600

    arow animation works

--- neo/index.js
+++ neo/index.js
@@ -1,6 +1,23 @@
+const arrowServiciosSubMenu = document.getElementById('servicios-submenu');
+
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
+
+
+arrowServiciosSubMenu.addEventListener('click', ()=> {
+  if(arrowServiciosSubMenu.classList.contains('arrow--left'))
+  {
+    arrowServiciosSubMenu.classList.remove('arrow--left');
+    arrowServiciosSubMenu.classList.add('arrow--down');
+  }
+  else
+  {
+    arrowServiciosSubMenu.classList.remove('arrow--down');
+    arrowServiciosSubMenu.classList.add('arrow--left');
+  }
+})
